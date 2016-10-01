6b6fa587e985007f96c3c2ddc3276529c5b5bb7e
adam kaczmarek <kaczmarek@oskarwegner.pl>
2016-10-01 23:34:09 +0200

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,40 @@ var lr = require('gulp-livereload');
 var preprocess = require('gulp-preprocess');
 var rename = require('gulp-rename');
 var minify = require('gulp-minify');
-var merge = require('merge-stream');
 var autoprefixer = require('gulp-autoprefixer');
+var connect = require('gulp-connect');
 
 var paths = {
     src: 'src',
-    dist: '../../web/apps/game'
+    dist: 'dist'
 };
+gulp.task('index_dev',function(){
+    gulp.src(paths.src+'/index.html')
+        .pipe(preprocess({
+                context: {
+                    ENV: "development"
+                }
+            }
+        ))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest(paths.dist))
+        .pipe(lr());
+})
+
+gulp.task('index',function(){
+    gulp.src(paths.src+'/index.html')
+        .pipe(preprocess({
+                context: {
+                    ENV: "production"
+                }
+            }
+        ))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest(paths.dist))
+        .pipe(lr());
+})
+
+
 
 gulp.task('assets', function () {
     gulp.src([paths.src + '/assets/**/*'])
@@ -19,45 +46,11 @@ gulp.task('assets', function () {
         .pipe(lr());
 });
 
-gulp.task('views', function () {
-    gulp.src([paths.src + '/js/guiapp/pages/**/*.html'])
-        .pipe(gulp.dest(paths.dist + "/views/pages/"))
-        .pipe(lr());
-});
-
-gulp.task('js-libs', function () {
+gulp.task('js', function () {
     gulp.src([
-        paths.src + '/libs/jquery.min.js',
-        paths.src + '/libs/angular.min.js',
-        paths.src + '/libs/ngUpload.js',
-        paths.src + '/libs/phaser.min.js',
-        paths.src + '/libs/phaser-debug.js',
-        paths.src + '/libs/bootstrap.js',
-        paths.src + '/libs/ui-bootstrap.min.js',
-        paths.src + '/libs/angular-animate.min.js',
-        paths.src + '/libs/angular-route.min.js',
-        paths.src + '/libs/angular-sanitize.min.js',
+        paths.src + '/js/*.js'
     ])
-        .pipe(concat('libs.min.js'))
-        .pipe(gulp.dest(paths.dist + '/libs'))
-
-    gulp.src([
-        paths.src + '/libs/scrollbars/*.*']
-    )
-        .pipe(gulp.dest(paths.dist + '/libs/scrollbars'))
-        .pipe(lr());
-});
-
-gulp.task('js-game', function () {
-    gulp.src([
-        paths.src + '/js/game/config.js',
-        paths.src + '/js/game/boot.js',
-        paths.src + '/js/game/preloader.js',
-        paths.src + '/js/game/menu.js',
-        paths.src + '/js/game/game.js',
-        paths.src + '/js/game/main.js'
-    ])
-        .pipe(concat('game.js'))
+        .pipe(concat('weather.js'))
         .pipe(minify({
             ext: {
                 min: '.min.js'
@@ -66,132 +59,31 @@ gulp.task('js-game', function () {
         .pipe(gulp.dest(paths.dist + '/js'))
 });
 
-gulp.task('js-game-dev', function () {
+gulp.task('js-dev', function () {
     gulp.src([
-        paths.src + '/js/game/*.js'
+        paths.src + '/js/*.js'
     ])
         .pipe(gulp.dest(paths.dist + '/dev/js'))
 });
 
-gulp.task('js-gui', function () {
+gulp.task('css', function () {
+
     gulp.src([
-            paths.src + '/js/guiapp/guiapp.js',
-            paths.src + '/js/guiapp/controllers.js',
-            paths.src + '/js/guiapp/pages/**/*.js',
-            paths.src + '/js/guiapp/filters.js',
-            paths.src + '/js/guiapp/services.js',
-            paths.src + '/js/guiapp/directives.js'
-        ]
-    )
-        .pipe(concat('guiapp.js'))
-        .pipe(rename('guiapp.min.js'))
-        .pipe(gulp.dest(paths.dist + '/js'))
-});
-
-gulp.task('js-gui-dev', function () {
-    gulp.src([
-        paths.src + '/js/guiapp/**/*']
-    )
-        .pipe(gulp.dest(paths.dist + '/dev/js'))
-        .pipe(lr());
-});
-
-gulp.task('js-all', ['js-libs', 'js-game', 'js-gui']);
-gulp.task('js-all-dev', ['js-libs', 'js-game-dev', 'js-gui-dev']);
-
-gulp.task('less-mobile', function () {
-    gulp.src(paths.src + '/js/less/mobile/*.less')
+        paths.src + '/less/*.less'
+    ])
         .pipe(less())
-        .pipe(rename('game-mobile.min.css'))
-        .pipe(gulp.dest(paths.src + '/css/mobile'))
-
-});
-
-gulp.task('cssMerge', function () {
-
-    var lessStream =
-        gulp.src([
-            paths.src + '/less/*.less',
-            paths.src + '/js/guiapp/pages/**/*.less'
-        ])
-            .pipe(less())
-            .pipe(autoprefixer())
-            .pipe(concat('game.min.css'));
-    //.pipe(gulp.dest(paths.src+'/css'))
-
-    var cssStream =
-        gulp.src([
-            paths.src + '/css/bootstrap.min.css',
-            paths.src + '/css/main.css'
-            //paths.src+'/css/game.min.css'
-        ])
-            .pipe(concat('all.css'))
-    //.pipe(gulp.dest(paths.dist+'/css'))
-
-
-    return merge(lessStream, cssStream)
-        .pipe(concat('all.min.css'))
-        .pipe(gulp.dest(paths.dist + '/css'))
-        .pipe(lr());
+        .pipe(autoprefixer())
+        .pipe(concat('weather.min.css'));
 
 })
-
-gulp.task('cssMobileMerge', function () {
-
-    var lessStream =
-        gulp.src([
-            paths.src + '/less/mobile/*.less',
-            paths.src + '/js/guiapp/pages/**/*.less'
-        ])
-            .pipe(less())
-            .pipe(autoprefixer())
-            .pipe(concat('game.css'));
-    //.pipe(gulp.dest(paths.src+'/css'))
-
-
-    var cssStream =
-        gulp.src([
-            paths.src + '/css/mobile/bootstrap.min.css',
-            paths.src + '/css/mobile/main.css'
-        ])
-            .pipe(concat('all.css'))
-
-
-    return merge(lessStream, cssStream)
-        .pipe(concat('mobile.min.css'))
-        .pipe(gulp.dest(paths.dist + '/css/mobile'))
-        .pipe(lr());
-
-})
-
-gulp.task('css-mobile', function () {
-    gulp.src([
-        paths.src + '/css/mobile/bootstrap.min.css',
-        paths.src + '/css/mobile/main.css',
-        paths.src + '/css/mobile/game-mobile.min.css'
-    ])
-        .pipe(concat('mobile.min.css'))
-        .pipe(gulp.dest(paths.dist + '/css/mobile'))
-        .pipe(lr());
-});
-
-gulp.task('css-all', ['cssMerge', 'cssMobileMerge']);
-
-gulp.task('copyLibs', function () {
-    gulp.src(paths.src + '/libs/**/*')
-        .pipe(gulp.dest(paths.dist + '/libs'))
-});
 
 gulp.task('watch', function () {
     lr.listen();
+    gulp.watch(
+        paths.src + '/js/*.js', ['js-dev']);
     gulp.watch([
-        paths.src + '/js/game/*.js',
-        paths.src + '/js/guiapp/**/*.js'], ['js-all-dev']);
-    gulp.watch([
-            paths.src + '/less/*.less',
-            paths.src + '/less/mobile/*.less',
-            paths.src + '/js/guiapp/pages/**/*.less',
-        ], ['css-all']
+            paths.src + '/less/*.less'
+        ], ['css']
     );
     gulp.watch([
         paths.src + '/assets/**/*'
@@ -199,7 +91,13 @@ gulp.task('watch', function () {
     gulp.watch([
         paths.src + '/js/guiapp/pages/**/*.html'
     ], ['views']);
+
+    connect.server({
+        root: 'dist',
+        port:8888,
+        livereload: true
+    });
 });
 
-gulp.task('prod', ['assets','views', 'js', 'css']);
-gulp.task('dev', ['assets', 'views','js-dev', 'css', 'watch']);
+gulp.task('prod', ['index','assets', 'js', 'css']);
+gulp.task('dev', ['index_dev','assets','js-dev', 'css', 'watch']);
---
+++ src/js/weather.js
@@ -0,0 +1,5 @@
+/**
+ * Created by bja on 01.10.16.
+ */
+
+var test = 4;
