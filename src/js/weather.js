aab9b3738bad29ea03d0be4c43a3f64ea046e93d
Adam Kaczmarek <kaczmarek@oskarwegner.pl>
2016-10-09 19:58:04 +0200

    sync

--- src/js/weather.js
+++ src/js/weather.js
@@ -3,8 +3,9 @@ var weatherWidget = (function () {
         getLocalization();
     };
 
-    var cityNameEl = document.getElementById("cityName");
+    var cityNameEl = document.getElementById("locationName");
     var forecastEl = document.getElementById("weatherForecast");
+    var currentTemp = document.getElementById("currentTemp");
 
     var myLocation = {
         lat:null,
@@ -15,30 +16,44 @@ var weatherWidget = (function () {
     function getLocalization() {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function(position) {
-                console.log(position);
-                getWeather('weather',position.coords.latitude,position.coords.longitude);
-                getWeather('forecast',position.coords.latitude,position.coords.longitude);
+                getWeather('weather',position.coords.latitude,position.coords.longitude,function (resp) {
+                    setCurrentWeather(resp)
+                });
+                getWeather('forecast',position.coords.latitude,position.coords.longitude,function (resp) {
+                    setForecastWeather(resp)
+                });
             });
         } else {
             alert('no geloloc')
         }
     }
 
-    function getWeather(type,lat,lng) {
+    function getWeather(type,lat,lng,callback) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/'+type+'?lat='+lat+'&lang=pl&lon='+lng+'&appid=1929fffb238baf259922bfbb99ae5a73');
+        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/'+type+'?lat='+lat+'&lang=pl&lon='+lng+'&appid=1929fffb238baf259922bfbb99ae5a73&units=metric');
         xhr.onload = function() {
             if (xhr.status === 200) {
                 var resp = JSON.parse(xhr.responseText);
-                console.log(resp);
-
-                cityNameEl.innerHTML = resp.name;
+                callback(resp);
             }
             else {
                 alert('Request failed.  Returned status of ' + xhr.status);
             }
         };
         xhr.send();
+    }
+
+    function setCurrentWeather(weatherData) {
+        cityNameEl.innerHTML = weatherData.name;
+        currentTemp.innerHTML = weatherData.main.temp;
+    }
+    function setForecastWeather(weatherData) {
+        var forecast = "<ul>";
+        weatherData.list.forEach(function (el) {
+            forecast+=  "<li>"+el.main.temp+"</li>";
+        });
+        forecast+="</ul>";
+        forecastEl.innerHTML = forecast;
     }
 
     return {
