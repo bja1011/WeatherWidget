9f34c4993f6ff0f33b737c09071ed12634236230
adam kaczmarek <kaczmarek@oskarwegner.pl>
2016-10-10 23:03:39 +0200

    wind, pressure

--- src/js/weather.js
+++ src/js/weather.js
@@ -4,7 +4,8 @@ var weatherWidget = (function () {
     };
 
     var config = {
-        units: 'metric'
+        units: 'metric',
+        currentWeatherData:null
     };
 
     var iconsEl;
@@ -20,7 +21,11 @@ var weatherWidget = (function () {
     var cityNameEl = document.getElementById("locationName");
     var forecastEl = document.getElementById("weatherForecast");
     var currentTemp = document.getElementById("currentTemp");
+    var currentWindDirectionEl = document.getElementById("currentWindDirection");
+    var currentWindSpeedEl = document.getElementById("currentWindSpeed");
+    var currentPressureEl = document.getElementById("currentPressure");
     var currentIconEl = document.getElementById("currentIcon");
+    var dateEl = document.getElementById("date");
 
     var myLocation = {
         lat:null,
@@ -31,8 +36,9 @@ var weatherWidget = (function () {
     function getLocalization() {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function(position) {
+
                 getWeather('weather',position.coords.latitude,position.coords.longitude,function (resp) {
-                    setCurrentWeather(resp)
+                    setCurrentWeather(resp);
                 });
                 getWeather('forecast/daily',position.coords.latitude,position.coords.longitude,function (resp) {
                     setForecastWeather(resp)
@@ -45,7 +51,7 @@ var weatherWidget = (function () {
 
     function getWeather(type,lat,lng,callback) {
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/'+type+'?lat='+lat+'&lang=pl&lon='+lng+'&appid=1929fffb238baf259922bfbb99ae5a73&units=metric');
+        xhr.open('GET', 'http://api.openweathermap.org/data/2.5/'+type+'?lat='+lat+'&lang=pl&lon='+lng+'&appid=1929fffb238baf259922bfbb99ae5a73&units='+config.units);
         xhr.onload = function() {
             if (xhr.status === 200) {
                 var resp = JSON.parse(xhr.responseText);
@@ -59,9 +65,14 @@ var weatherWidget = (function () {
     }
 
     function setCurrentWeather(weatherData) {
+        config.currentWeatherData = weatherData;
+        dateEl.innerHTML = formatDate(weatherData.dt);
         cityNameEl.innerHTML = weatherData.name;
-        currentTemp.innerHTML = showTemperature(weatherData.main.temp);
+        currentTemp.innerHTML = formatTemperature(weatherData.main.temp);
         currentIconEl.innerHTML = getIcon(weatherData.weather[0].icon)
+        currentPressureEl.innerHTML = formatPressure(weatherData.main.pressure);
+        currentWindDirectionEl.style.transform = 'rotate('+weatherData.wind.deg+'deg)';
+        currentWindSpeedEl.innerHTML = formatWindSpeed(weatherData.wind.speed);
 
     }
 
@@ -69,17 +80,38 @@ var weatherWidget = (function () {
         var forecast = "<ul>";
         weatherData.list.forEach(function (el) {
             var date = new Date(el.dt*1000);
-            var month = parseInt(date.getMonth())+1;
             var day = parseInt(date.getDate());
-            forecast+=  "<li><span class='date'>"+day+"-"+month+"</span><span class='date'>"+el.temp.max+"</span>"+getIcon(el.weather[0].icon)+"</li>";
+            forecast+=  "<li><span class='date'>"+day+"</span> <span class='temp'>"+formatTemperature(el.temp.max)+"</span>"+getIcon(el.weather[0].icon)+"</li>";
         });
         forecast+="</ul>";
         forecastEl.innerHTML = forecast;
     }
     
-    function showTemperature(value) {
+    function formatTemperature(value) {
         var unit = config.units=="metric"?"&#778;C":"&#778;F";
         return Math.round(value)+" "+unit;
+    }
+
+    function formatPressure(value) {
+        return Math.round(value)+' hPa';
+    }
+
+    function formatWindSpeed(value) {
+        var unit = config.units=="metric"?"m/s":"m/h";
+        return Math.round(value)+" "+unit;
+
+    }
+
+    function formatDate(date) {
+        var date = new Date(date*1000);
+        var day = parseInt(date.getDate());
+        var month = parseInt(date.getMonth());
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+
+        return day+" "+monthNames[month];
+
     }
 
     //load icons and create
@@ -96,15 +128,11 @@ var weatherWidget = (function () {
             }
         };
         xhr.send();
-
-
     }
 
     function getIcon(iconId) {
         return iconsEl.querySelectorAll("#icon-"+iconId)[0].outerHTML;
     }
-
-
 
     return {
         run:run
